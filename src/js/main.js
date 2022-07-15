1f865fb53956767b0975a75796e1e538f016317a
saramonzon22 <sara.mon.quesada@gmail.com>
2022-07-15 20:37:02 +0100

    added styles and js

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ const searchButton = document.querySelector('.button-js-search');
 const resetButton = document.querySelector('.button-js-reset');
 const inputSearch = document.querySelector('.input-search-js');
 const searchResult = document.querySelector('.search-js');
+const containerSearch = document.querySelector('.container-search-js');
 
 let animeSearchList = [];
 let animeSearchFav = [];
@@ -11,28 +12,27 @@ let animeSearchFav = [];
 function resetSearch(event) {
   event.preventDefault();
   inputSearch.value = '';
+
 }
 /* function searchFunction (event){
   event.preventDefault();
 } */
-const handleUp = (event) => {
+const handleUp = () => {
   const inputSearchUp = inputSearch.value.toLowerCase();
-  const inputSearchFilter = animeSearchList.filter((newSearch) => newSearch.title_english.toLowerCase().includes(inputSearchUp));
+  const inputSearchFilter = animeSearchList.filter((newSearch) => newSearch.title.toLowerCase().includes(inputSearchUp));
   renderAnime(inputSearchFilter);
 };
 
-inputSearch.addEventListener('keyup', handleUp);
+
 
 function searchAnime() {
-  fetch(
-    'https://api.jikan.moe/v4/anime'
-  )
+  const inputSearchAnime = inputSearch.value;
+  fetch(`https://api.jikan.moe/v4/anime?q=${inputSearchAnime}`)
     .then((response) => response.json())
     .then((animeSearch) => {// data solo existe en esta variable local
       animeSearch = animeSearch.data;
       renderAnime(animeSearch);
     });
-
 }
 
 
@@ -41,19 +41,18 @@ function searchAnime() {
 function renderAnime(animeSearch) {
   let html = '';
   for (const singleAnime of animeSearch) {
-    html += `<li class="list-anime list-anime-js">`;
-    html += ` <h2>'${singleAnime.title}'</h2>`;
-    html += ` <img src='${singleAnime.images.jpg.small_image_url}'>`;
-    html += `</li>`;
-  }
+    if (singleAnime.images.jpg.small_image_url !== 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png') {
+      html += `<li class="list-anime list-anime-js"><h2>'${singleAnime.title}'</h2><img src=${singleAnime.images.jpg.small_image_url}></li>`;
+    } else {
+      html += `<li class="list-anime list-anime-js"><h2>'${singleAnime.title}'</h2><img src='https://via.placeholder.com/210x295/ffffff/666666/?text=TV'></li>`;
+    }
 
-  /*  si imagen es !== ${singleAnime.images.jpg.small_image_url}
-    entonces src = https://via.placeholder.com/210x295/ffffff/666666/?text=TV. */
+  }
   searchResult.innerHTML = html;
 
 }
 
 resetButton.addEventListener('click', resetSearch);
-inputSearch.addEventListener('keyup', searchAnime);
+inputSearch.addEventListener('keyup', searchAnime, handleUp);
 
 
